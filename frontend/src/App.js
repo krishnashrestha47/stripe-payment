96a6b7a84d2f430e5944cefd5f88bcd9d6f30e5f
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-08-13 23:20:33 +1000

    stripe implementation on the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import "./App.css";
 import CheckoutForm from "./components/CheckoutForm";
+
+const publishableKey = process.env.REACT_APP_PK;
+const stripePromise = loadStripe(publishableKey);
 
 function App() {
   return (
     <div className="App">
-      <CheckoutForm />
+      <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
     </div>
   );
 }
--- frontend/src/components/CheckoutForm.js
+++ frontend/src/components/CheckoutForm.js
@@ -1,16 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import axios from "axios";
 
 const CheckoutForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const price = 500;
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+
+    try {
+      const result = await axios({
+        url: "http://localhost:8000/api/payment/create-payment-intent",
+        method: "POST",
+        data: {
+          amount: price * 100,
+          currency: "aud",
+          paymentMethodType: "card",
+        },
+      });
+      console.log(result);
+
+      const clientSecret = result.data.clientSecret;
+      const { paymentIntent } = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+          billing_details: {
+            name,
+            email,
+          },
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="payment">
-      <form>
+      <form onSubmit={handleOnSubmit}>
         <div>
           <label>Name</label>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            value={name}
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <div>
           <label>Email</label>
-          <input type="email" name="email" />
+          <input
+            type="email"
+            value={email}
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <CardElement options={{ hidePostalCode: true }} />
         </div>
         <button type="submit">Pay Now</button>
       </form>
